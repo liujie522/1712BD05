25c62fa06d639e6a7ed38706bf34e3b5d9016b3c
Liu Jie <liujie@qq.com>
2020-03-25 15:40:57 +0800

    第二次提交

--- src/test/java/com/liujie/test/Test01.java
+++ src/test/java/com/liujie/test/Test01.java
@@ -3,6 +3,6 @@ package com.liujie.test;
 public class Test01 {
 public static void main(String[] args) {
 	System.out.println("helloWord");
-	System.out.println("HELLOW,第二次");
+	System.out.println("HEL,第二次");
 }
 }
